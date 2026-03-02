bb0c0ad01e5d089c46d07284501ec3b20d328f25
aurum-codes <swarnadeep.c17@gmail.com>
2026-03-02 09:00:18 +0530

    Refactor UC6 class and add methods for letter patterns

--- UC-6.java
+++ UC-6.java
@@ -1,42 +1,55 @@
-import java.util.Collections;
+public class UC6 {
 
-public class UC5 {
+    // Static method to build pattern for letter 'O'
+    static String[] buildO() {
+        return new String[] {
+            " OOO ",
+            "O   O",
+            "O   O",
+            "O   O",
+            " OOO "
+        };
+    }
+
+    // Static method to build pattern for letter 'P'
+    static String[] buildP() {
+        return new String[] {
+            "PPP  ",
+            "P  P ",
+            "PPP  ",
+            "P    ",
+            "P    "
+        };
+    }
+
+    // Static method to build pattern for letter 'S'
+    static String[] buildS() {
+        return new String[] {
+            " SSS ",
+            "S    ",
+            " SSS ",
+            "    S",
+            " SSS "
+        };
+    }
 
     public static void main(String[] args) {
 
-        // UC5: Array declaration + initialization in a SINGLE statement
-        // String.join("", Collections.nCopies(n, ch)) repeats character ch n times
-        String[] bannerLines = {
+        // Build each character pattern using static methods
+        String[] O = buildO();
+        String[] P = buildP();
+        String[] S = buildS();
 
-            // Line 1: Top border (40 stars)
-            String.join("", Collections.nCopies(40, "*")),
+        // Banner: O P S
+        String[][] banner = { O, P, S };
 
-            // Line 2: Empty padded line
-            "*" + String.join("", Collections.nCopies(38, " ")) + "*",
-
-            // Line 3: Welcome message centered in 40 chars
-            "*" + String.join("", Collections.nCopies(11, " "))
-                + "WELCOME TO OOPS"
-                + String.join("", Collections.nCopies(12, " ")) + "*",
-
-            // Line 4: Sub-title centered in 40 chars
-            "*" + String.join("", Collections.nCopies(7, " "))
-                + "Java Banner Application"
-                + String.join("", Collections.nCopies(8, " ")) + "*",
-
-            // Line 5: Empty padded line
-            "*" + String.join("", Collections.nCopies(38, " ")) + "*",
-
-            // Line 6: Empty padded line
-            "*" + String.join("", Collections.nCopies(38, " ")) + "*",
-
-            // Line 7: Bottom border (40 stars)
-            String.join("", Collections.nCopies(40, "*"))
-        };
-
-        // Enhanced for loop: iterate through each line and print it
-        for (String line : bannerLines) {
-            System.out.println(line);
+        // Render banner line by line
+        for (int line = 0; line < 5; line++) {
+            StringBuilder sb = new StringBuilder();
+            for (String[] ch : banner) {
+                sb.append(ch[line]).append("  ");
+            }
+            System.out.println(sb.toString());
         }
     }
 }
